721a477ca09323fe3c6c709374647ae7a3218574
Wen Zhang <iwenyou@gmail.com>
2017-02-28 18:04:46 -0800

    stucked

--- VasyaClerk.js
+++ VasyaClerk.js
@@ -1,4 +1,23 @@
+
+
 function tickets(peopleInLine){
   // ...
-  
+  cash = 0;
+
+  for(var i = 0; i < peopleInLine.length; i++){
+    if(peopleInLine[i] - 25 <= cash){
+      cash = cash - (peopleInLine[i] - 25);
+      console.log(cash);
+    }else if(peopleInLine[i] - 25 > cash){
+      console.log("NO");
+
+      return ('NO');
+    }
+  }
+  console.log("YES");
+
+  return ('YES');
+
 }
+
+tickets([25, 25, 50, 50]);
